c19d1dc9c07d06feac2b99211efc089ca7eaab2d
Erik <erik.agayan1@gmail.com>
2024-08-04 16:54:24 +0300

    Add new urls and migrations

--- sms/urls.py
+++ sms/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import UserViewSet, ListViewSet
+from sms.views import UserViewSet, ListViewSet
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="user")
